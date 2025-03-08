5e591b470cf8c36c1550d8f4afcfe7454b9553f3
Sachin <sachinkaushikofficial@gmail.com>
2025-03-08 13:41:11 +0530

    Initial commit

---
+++ src/main/java/com/sachinsk/api_gateway_jb/ApiGatewayJbApplication.java
@@ -0,0 +1,13 @@
+package com.sachinsk.api_gateway_jb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayJbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayJbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sachinsk/api_gateway_jb/ApiGatewayJbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sachinsk.api_gateway_jb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayJbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
